a533ae8f6e229565abad7de576473723211ee73b
Jonathan Lurie <jlurie@ace-ws-50.cbrain.mcgill.ca>
2017-04-07 17:20:10 -0400

    buggy

--- js/shaders.js
+++ js/shaders.js
@@ -23,27 +23,28 @@ shaders.vertex = `
 /* ***************** FRAGMENT SHADER **************************************** */
 
 shaders.fragment = `
+  precision lowp float;
   
   // a max number we allow, can be upt to 16
   const int maxNbOfTextures = 1;
   
   // Number of texture used with this dataset
   // cannot be higher than maxNbOfTextures
-  uniform int nbOfTextureUsed;
+  uniform float nbOfTextureUsed;
   
   // size of the mosaic
-  uniform int nbSlicePerRow;
-  uniform int nbSlicePerCol;
+  uniform float nbSlicePerRow;
+  uniform float nbSlicePerCol;
   // not necessary equal to nbSlicePerRow*nbSlicePerCol because last line
   // is not necessary full
-  uniform int nbSliceTotal;
+  uniform float nbSliceTotal;
   
-  uniform int indexSliceToDisplay;
+  uniform float indexSliceToDisplay;
   
   // space length
-  uniform int xspaceLength;
-  uniform int yspaceLength;
-  uniform int zspaceLength;
+  uniform float xspaceLength;
+  uniform float yspaceLength;
+  uniform float zspaceLength;
   
   // a texture will contain a certain number of slices
   uniform sampler2D textures[maxNbOfTextures];
@@ -53,31 +54,52 @@ shaders.fragment = `
   varying  vec4 worldCoord;
   varying  vec2 vUv;
 
-  
+  float myMod(float x, float y){
+    return x - (y * floor(x/y));
+  }
   
   void main( void ) {
+    
+    
     // the position within the shader
     vec2 shaderPos = vUv;
     
-    // row/col index of the slice within the grid of slices
-    int rowTexture = indexSliceToDisplay / nbSlicePerCol;
-    int colTexture = int(mod( float(indexSliceToDisplay), float(nbSlicePerCol) ) );
-    
     // step to jump from a slice to another on a unit-sized texture
-    float stepWidth = 1.0 / float(nbSlicePerRow);
-    float stepHeight = 1.0 / float(nbSlicePerCol);
+    float sliceWidth = 1.0 / nbSlicePerRow;
+    float sliceHeight = 1.0 / nbSlicePerCol;
     
-    // step to jump from one pixel to another in a unit-sized texture
-    float microStepSliceWidth = float(stepWidth) / float(xspaceLength);
-    float microStepSliceHeight = float(stepHeight) / float(yspaceLength);
+    // row/col index of the slice within the grid of slices
+    float rowTexture = nbSlicePerCol - 1.0 - floor(indexSliceToDisplay / nbSlicePerRow);
+    float colTexture = myMod( indexSliceToDisplay, nbSlicePerRow );
     
+    float indexSliceToDisplay2 = indexSliceToDisplay;
+    float nbSlicePerRow2 = nbSlicePerRow;
+    gl_FragColor = vec4(myMod(indexSliceToDisplay2 ,nbSlicePerRow2) / 23.0, 0.0 , 0.0, 1.0);
+    return;
+    
+    if( (indexSliceToDisplay == 23.0) && (nbSlicePerRow == 23.0) &&  ( myMod( floor(indexSliceToDisplay), floor(nbSlicePerRow) ) == 23.0 ) ){
+    //if( mod( 23.0, nbSlicePerRow ) == 23.0 ){
+      gl_FragColor = vec4(1.0, 0.0 , 0.0, 1.0);
+      return;
+    }
+    
+    /*
+    if( colTexture == nbSlicePerRow ){
+      colTexture = 0.0;
+      rowTexture = rowTexture + 1.0;
+    }
+    */
+  
     vec2 posInTexture = vec2(
-      stepWidth * float(colTexture) + vUv.x * stepWidth ,
-      stepHeight * float(rowTexture) + vUv.y * stepHeight
+      sliceWidth * colTexture + vUv.x * sliceWidth ,
+      sliceHeight * rowTexture + vUv.y * sliceHeight
     );
     
-    //gl_FragColor = vec4(1.0, 0.2 , 0.8, 1.0);
-    //gl_FragColor = texture2D(textures[0], posInTexture);
+  
+    
+
+    gl_FragColor = texture2D(textures[0], posInTexture);
+    //gl_FragColor = texture2D(textures[0], vUv);
     //gl_FragColor = vec4(float(indexSliceToDisplay) / 256., 0.0 , 0.0, 1.0);
   }
 
