6e93f11fe2d101dac210af733cb28b8751d8eeaa
Jonathan Lurie <jlurie@ace-ws-50.cbrain.mcgill.ca>
2017-04-10 16:47:55 -0400

    ADD world example with oblique on shader

--- js/shaders.js
+++ js/shaders.js
@@ -96,3 +96,94 @@ shaders.fragment = `
   }
 
 `
+
+
+
+shaders.fragmentWorld = `
+  precision highp float;
+  
+  // a max number we allow, can be upt to 16
+  const int maxNbOfTextures = 1;
+  
+  // Number of texture used with this dataset
+  // cannot be higher than maxNbOfTextures
+  uniform float nbOfTextureUsed;
+  
+  // size of the mosaic
+  uniform float nbSlicePerRow;
+  uniform float nbSlicePerCol;
+  // not necessary equal to nbSlicePerRow*nbSlicePerCol because last line
+  // is not necessary full
+  uniform float nbSliceTotal;
+  
+  // space length
+  uniform float xspaceLength;
+  uniform float yspaceLength;
+  uniform float zspaceLength;
+  
+  // a texture will contain a certain number of slices
+  uniform sampler2D textures[maxNbOfTextures];
+  
+  
+  // Shared with the vertex shader
+  varying  vec4 worldCoord;
+  varying  vec2 vUv;
+
+  float myMod(float x, float y){
+    return x - (y * float(int(x/y)));
+  }
+  
+  /**
+  * Returns accurate MOD when arguments are approximate integers.
+  */
+  float modI(float a,float b) {
+      float m = a - floor( ( a + 0.5 ) / b) * b;
+      return floor( m + 0.5 );
+  }
+
+  
+    
+  void main( void ) {
+
+    // worldCoord is [0, n], but the box is centered on 0 to make rotation work better
+    // so worldCoordShifted is like worldCoord but shifted of half size in each direction
+    vec3 worldCoordShifted = vec3( worldCoord.x + xspaceLength/2.0, worldCoord.y + yspaceLength/2.0, worldCoord.z + zspaceLength/2.0);
+    
+    if(worldCoordShifted.x < 0.0 || worldCoordShifted.x > xspaceLength ||
+      worldCoordShifted.y < 0.0 || worldCoordShifted.y > yspaceLength ||
+      worldCoordShifted.z < 0.0 || worldCoordShifted.z > zspaceLength)
+    {
+        //gl_FragColor = vec4(0.0, 0.0, 0.0, 0.2);
+        discard;
+        return;
+    }
+      
+    // the position within the shader
+    vec2 shaderPos = vUv;
+    
+    // step to jump from a slice to another on a unit-sized texture
+    float sliceWidth = 1.0 / nbSlicePerRow;
+    float sliceHeight = 1.0 / nbSlicePerCol;
+    
+     
+    
+    float indexSliceToDisplay = floor(worldCoordShifted.z + 0.5);
+    
+    
+    // row/col index of the slice within the grid of slices
+    // (0.5 rounding is mandatory to deal with float as integers)
+    float rowTexture = nbSlicePerCol - 1.0 - floor( (indexSliceToDisplay + 0.5) / nbSlicePerRow);
+    float colTexture = modI( indexSliceToDisplay, nbSlicePerRow );
+    
+    float indexSliceToDisplay2 = indexSliceToDisplay + 0.0;
+    float nbSlicePerRow2 = nbSlicePerRow;
+    
+    vec2 posInTexture = vec2(
+      sliceWidth * colTexture + worldCoordShifted.x/xspaceLength * sliceWidth ,
+      sliceHeight * rowTexture + worldCoordShifted.y/yspaceLength * sliceHeight
+    );
+    
+    gl_FragColor = texture2D(textures[0], posInTexture);
+  }
+
+`
